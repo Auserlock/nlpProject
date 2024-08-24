bac47e8e2ddf4d510b95257a8f7d315ad09cb344
arce <arce.domartic@gmail.com>
2024-08-24 12:32:28 +0800

    模型建构

---
+++ code/lr_config.py
@@ -0,0 +1,14 @@
+import os
+
+pwd_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+class LrConfig(object):
+    #  训练模型用到的路径
+    dataset_path = os.path.join(pwd_path + r'/data' + r"/train.tsv")  # linux 文件路径, windows可能需要更改
+    testdata_path = os.path.join(pwd_path + r'/data' + r"/test.tsv")
+    model_save_path = os.path.join(pwd_path + r'/model' + r"/classification_model.keras")
+    predict_save_path = os.path.join(pwd_path + r'/data' + r"/submission.tsv")
+
+    #  变量
+    num_epochs = 20
