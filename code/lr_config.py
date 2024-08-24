40232d567fcc3511fdc6cf66712c05bb097d7fcc
arce <arce.domartic@gmail.com>
2024-08-24 12:33:57 +0800

    .

--- code/lr_config.py
+++ code/lr_config.py
@@ -8,7 +8,7 @@ class LrConfig(object):
     dataset_path = os.path.join(pwd_path + r'/data' + r"/train.tsv")  # linux 文件路径, windows可能需要更改
     testdata_path = os.path.join(pwd_path + r'/data' + r"/test.tsv")
     model_save_path = os.path.join(pwd_path + r'/model' + r"/classification_model.keras")
-    predict_save_path = os.path.join(pwd_path + r'/data' + r"/submission.tsv")
+    predict_save_path = os.path.join(pwd_path + r'/data' + r"/submission.csv")
 
     #  变量
     num_epochs = 20
